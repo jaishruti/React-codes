87e3708cd9e92fd208194dd37b1e0faee7d5a931
jaishruti <shrutijaiswal223@gmail.com>
2023-12-13 14:05:22 +0530

    Product imported and used

--- src/App.js
+++ src/App.js
@@ -2,12 +2,32 @@
 import "./App.css";
 import Counter from "./Counter.js";
 import Form from "./Form.js";
+import Product from "./Product.js";
+
+const data = [
+  {
+    name: "Nirma",
+    price: "800",
+  },
+  {
+    name: "soap",
+    price: "1000",
+  },
+];
+
+// definig products here
+// {/* rendering product from Product component */}
+let products = data.map((ele) => {
+  return <Product name={ele.name} price={ele.price} />;
+});
 
 function App() {
   return (
-    <div className="App">
+    <div className="App w-full flex flex-col gap-4 items-center ">
       <Counter />
       <Form />
+      {/* //display products */}
+      {products}
     </div>
   );
 }
