65c78abeb9f38da5915b1cb7820dcfd507db52d9
jaishruti <shrutijaiswal223@gmail.com>
2023-12-13 13:00:18 +0530

    Form component omported

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 // import logo from './logo.svg';
 import "./App.css";
 import Counter from "./Counter.js";
-import Counter from "./Counter.js";
+import Form from "./Form.js";
 
 function App() {
   return (
     <div className="App">
       <Counter />
-      <Form.js/>
+      <Form />
     </div>
   );
 }
--- src/Form.js
+++ src/Form.js
@@ -1,16 +1,35 @@
 import { useState } from "react";
 
 export default function Form() {
+  // handle any input on enter of each character
   const handleChange = (event) => {
     setInput(event.target.value);
+  };
+
+  //onpress of clear button remove all from input
+  const handleClear = () => {
+    setInput("");
   };
   const [inputData, setInput] = useState("");
   return (
     <div>
       <label>
         First name:
-        <input type="text" value={inputData} onChange={handleChange} />
+        <input
+          type="text"
+          value={inputData}
+          onChange={handleChange}
+          placeholder="Enter your name"
+          className="border-solid border-black border-[2px]"
+        />
+        <button
+          className="m-1 p-2 bg-gray-500 text-white "
+          onClick={handleClear}
+        >
+          clear
+        </button>
       </label>
+
       {inputData !== "" && <p>You entered {inputData}</p>}
     </div>
   );
