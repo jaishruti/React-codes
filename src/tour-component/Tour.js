07307588d0f5152318cc09f159a52c6beabebe17
jaishruti <shrutijaiswal223@gmail.com>
2023-12-16 00:20:07 +0530

    changed name of func to removeTour and changed the function def

--- src/tour-component/Tour.js
+++ src/tour-component/Tour.js
@@ -6,15 +6,15 @@ export default function Tour() {
   const [dataTour, setdataTour] = useState(tours);
 
   //receiving prop from child(child->parent) : lifting up
-  const handleNotinterested = (id) => {
-    const tourAfterRemove = tours.filter((d) => d.id !== id);
+  function removeTour(id) {
+    const tourAfterRemove = dataTour.filter((d) => d.id !== id);
     console.log(tourAfterRemove);
     setdataTour(tourAfterRemove);
-  };
+  }
   return (
     <div className="flex flex-col gap-[1.5rem]">
       {dataTour.map((data) => {
-        return <TourCard {...data} func={handleNotinterested}></TourCard>;
+        return <TourCard {...data} removeTour={removeTour}></TourCard>;
       })}
     </div>
   );
